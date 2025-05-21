7cae1796ecf4d8ca2ed89107b87fb75c029c7d5f
Trisha <134681506+trisha123shetty@users.noreply.github.com>
2025-05-21 21:34:57 +0530

    Add files via upload

---
+++ Recurrsion/Permutation.java
@@ -0,0 +1,18 @@
+public class Permutation {
+    public static void printPermutation(String ans, String s){
+        if(s.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            String left=s.substring(0,i);
+            String right= s.substring(i+1);
+            printPermutation(ans+ch,left+right);
+        }
+    }
+    public static void main(String[] args) {
+        String s ="abc";
+        printPermutation(" ",  s);
+    }
+}
